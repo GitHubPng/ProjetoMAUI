a42bb028bf3b7837c2ac8c11e76d7858f6ea267b
Marcos Rafael de Souza Mello / TRUE <marcos.mello@true.com.br>
2025-11-13 09:51:23 -0300

    Novos projetos

---
+++ AppNavigationPage/Page3.xaml.cs
@@ -0,0 +1,11 @@
+namespace AppNavigationPage;
+
+public partial class Page3 : ContentPage {
+    public Page3() {
+        InitializeComponent();
+    }
+
+    private void OnButtonPreviousClicked(object sender, EventArgs e) {
+        Navigation.PopAsync();
+    }
+}
