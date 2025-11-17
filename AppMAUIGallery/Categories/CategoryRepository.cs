28526a699d524c5a74e9e8878195c2537b94f882
marcos.mello <marcos406016@gmail.com>
2025-11-17 10:46:07 -0300

    Novas alterações no AppGallery
    Atualiza layouts e adiciona repositório de categorias
    
    Removidos arquivos antigos de AbsoluteLayout do projeto e
    adicionados novos arquivos XAML para AbsoluteLayoutPage,
    FlexLayoutPage, GridLayout e StackLayoutPage. Alterações no
    Menu.xaml para incluir ScrollView e navegação por toque.
    Implementada lógica de carregamento dinâmico de categorias e
    componentes no Menu.xaml.cs. Adicionados novos arquivos para
    definir a estrutura e comportamento das páginas de layout.
    Criadas classes Category e Component para gerenciar dados de
    categorias e componentes.

---
+++ AppMAUIGallery/Categories/CategoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using AppMAUIGallery.Models;
+using AppMAUIGallery.Views.Layouts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMAUIGallery.Categories {
+    internal class CategoryRepository {
+
+        public CategoryRepository() { }
+        public List<Category> GetCategories() {
+            List<Category> categories = new List<Category>();
+            categories.Add(new Category {
+                Name = "Layout",
+                Components = new List<Component> {
+                    new Component {
+                        Title = "StackLayout",
+                        Description = "Organização sequencial dos elementos.",
+                        Page = typeof(StackLayoutPage)
+                    },
+                    new Component {
+                        Title = "Grid",
+                        Description = "Organiza os elementos dentro de uma tabela.",
+                        Page = typeof(GridLayout)
+
+                    },
+
+                    new Component {
+                        Title = "AbsoluteLayout",
+                        Description = "Liberdade total para posicionar e dimensionar os elementos na tela.",
+                        Page = typeof(AbsoluteLayoutPage)
+
+                    },
+                    new Component {
+                        Title = "FlexLayout",
+                        Description = "Organiza os elementos de forma sequencial com muitas opções de personalização.",
+                        Page = typeof(FlexLayoutPage)
+
+                    }
+                }
+            });
+
+            return categories;
+        }
+    }
+}
