adccd786c72672736c757f58064dd8a9dd87aaf6
marcos.mello <marcos406016@gmail.com>
2025-12-19 13:27:03 -0300

    Criação do AppTask e arrumando DB com migrations, Models.

---
+++ AppTask.Database/AppTaskContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTask.Database
+{
+    public class AppTaskContext : DbContext
+    {
+        public DbSet<>
+
+    }
+}
