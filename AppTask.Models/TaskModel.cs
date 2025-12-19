799c53aa363f9d764e3a7bcc3974062e4e7c20fc
marcos.mello <marcos406016@gmail.com>
2025-12-19 13:27:03 -0300

    Finalizado organização inicial do AppGallery, agora criando o AppTask e arrumando DB com migrations.

---
+++ AppTask.Models/TaskModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AppTask.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime PrevisionDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public List<SubTaskModel> SubTasks { get; set; } = new List<SubTaskModel>();
+    }
+
+}
---
+++ AppTask/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using AppTask.Views;
+using Microsoft.Maui.Platform;
+
+namespace AppTask
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+
+            CustomHandler();
+
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new StartPage());
+
+        }
+        private void CustomHandler()
+        {
+            EntryNoBorder();
+            DatePickerNoBorder();
+        }
+
+        private static void EntryNoBorder()
+        {
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoBorder", (handler, view) =>
+            {
+#if ANDROID
+                //ANDROID
+                handler.PlatformView.BackgroudTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+
+#elif IOS || MACCATALYST
+                //IOS || MACCATALYST
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+                //WINDOWS - Não funciona 100%
+                handler.PlatformView.BorderThickness = new Thickness(0).ToPlatform();
+#endif
+            });
+        }
+        private static void DatePickerNoBorder()
+        {
+            Microsoft.Maui.Handlers.DatePickerHandler.Mapper.AppendToMapping("NoBorder", (handler, view) =>
+            {
+#if ANDROID
+                //ANDROID
+                handler.PlatformView.BackgroudTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+
+#elif IOS || MACCATALYST
+                //IOS || MACCATALYST
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+                //WINDOWS - Não funciona 100%
+                handler.PlatformView.BorderThickness = new Thickness(0).ToPlatform();
+#endif
+            });
+        }
+    }
+}
